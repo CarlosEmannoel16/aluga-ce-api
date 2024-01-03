e4ca72f3b938b45116f9eeefe736840a119d52af
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-01-03 08:42:34 -0300

    feat: add methods in class client

--- src/domain/client/entity/client.ts
+++ src/domain/client/entity/client.ts
@@ -1,9 +1,13 @@
+import { Address } from "../value-object/address";
+
 export class Client {
   private _id: string;
   private _name: string;
   private _email: string;
   private _phone: string;
   private _cpf: string;
+  private _password!: string;
+  private _address!: Address;
 
   constructor(
     id: string,
@@ -17,12 +21,57 @@ export class Client {
     this._email = email;
     this._phone = phone;
     this._cpf = cpf;
+
+    this.validate();
   }
 
   validate(): void {
-    if (!this._name) throw new Error("Name is required");
-    if (!this._email) throw new Error("Email is required");
-    if (!this._phone) throw new Error("Phone is required");
-    if (!this._cpf) throw new Error("CPF is required");
+    const errors: string[] = [];
+    if (!this._name) errors.push("[Client]: Name is required");
+    if (!this._email) errors.push("[Client]: Email is required");
+    if (!this._phone) errors.push("[Client]: Phone is required");
+    if (!this._cpf) errors.push("[Client]: Cpf is required");
+
+    if (errors.length) throw new Error(errors.join(", "));
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get phone(): string {
+    return this._phone;
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  changePassword(password: string): void {
+    this._password = password;
+  }
+
+  set password(password: string) {
+    this._password = password;
+  }
+
+  set address(address: Address) {
+    this._address = address;
+  }
+
+  get address(): Address {
+    return this._address;
   }
 }
---
+++ src/domain/client/value-object/address.ts
@@ -0,0 +1,37 @@
+export class Address {
+  _street: string;
+  _number: number;
+  _complement: string;
+  _zipCode: string;
+  _city: string;
+
+  constructor(
+    street: string,
+    number: number,
+    complement: string,
+    zipCode: string,
+    city: string
+  ) {
+    this._street = street;
+    this._number = number;
+    this._complement = complement;
+    this._zipCode = zipCode;
+    this._city = city;
+
+    this.validate();
+  }
+
+  validate(): void {
+    const errors: string[] = [];
+    if (!this._street) errors.push("[Address]: Street is required");
+    if (!this._number) errors.push("[Address]: Number is required");
+    if (!this._zipCode) errors.push("[Address]: ZipCode is required");
+    if (!this._city) errors.push("[Address]: City is required");
+
+    if (errors.length) throw new Error(errors.join(", "));
+  }
+
+  toString(): string {
+    return `${this._street}, ${this._number} - ${this._city}`;
+  }
+}
